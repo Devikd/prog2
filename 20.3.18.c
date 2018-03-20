6b305df73b757cb2b400cce7a560f9157c6d4ed2
Devikd <devikumar1998@yahoo.com>
2018-03-20 15:45:41 +0530

    Create 20.3.18.c

---
+++ 20.3.18.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int isLowercaseVowel,isUppercaseVowel;
+printf("Enter an alphabet:");
+scanf("%c",&c);
+isLowercaseVowel=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+isUppercaseVowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+if(isLowercaseVowel||isUppercaseVowel)
+{
+printf("%c is vowel.",c);
+}
+else
+{
+printf("%c is consonant.",c);
+}
+return 0;
+}
