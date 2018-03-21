7514249a8b9071560f600c44d8d670633ed583ae
Devikd <devikumar1998@yahoo.com>
2018-03-21 15:43:46 +0530

    Create 20.3.18.2.py

---
+++ 20.3.18.2.py
@@ -0,0 +1,9 @@
+num = input('Enter any Number:')
+try:
+  val=int(num)
+  ifnum == str(num)[::-1]:
+     print ('the  given Number is PALINDROME')
+  else:
+    print('the  given Number is notPalindrome'):
+except valueError:
+    print("That' s not a valid number, Try Again!")
